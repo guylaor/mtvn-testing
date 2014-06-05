1c4b93ee88791569fbd7ad76471f121eee448e0f
guylaor <gopala.das@gmail.com>
2014-06-05 16:27:00 -0400

    Create sizmek_html5_728x90_videowall.js

---
+++ Bento-iOS/ads/sizmek_html5_728x90_videowall.js
@@ -0,0 +1,42 @@
+[
+	{
+		"config" : {
+			"isCoppaCompliant" : false,
+			"audienceManagerEnabled" : false,
+		},
+		"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 728,
+			"height" : 90,
+			"appName" : "btg_test_app",
+			"sections" : "",
+			"isDFP" : false,
+			"enableCloseButton" : false,
+			"closeButtonInterval" : 5,
+			"isRefreshable" : false,
+			"refreshInterval" : 10,
+			"keyValues" : {
+				"!category" : "v1.2.3",
+				"ver"   : "v2.0",
+				"testmode" : "sizmek_html5_728x90_videowall"
+			},
+			"keychainKeyValues" : {
+				"aamSegments" : ""
+			},
+			"dfpData" : {
+				"metadata" : {
+					"property" : "mtv_news",
+					"hub" : "breaking",
+					"is_main" : true
+				},
+				"mappings" : {
+					"ad-unit" : {
+						"1" : "@property",
+						"2" : "@hub"
+					}
+				}		        	
+			}
+		}
+	}
+]
